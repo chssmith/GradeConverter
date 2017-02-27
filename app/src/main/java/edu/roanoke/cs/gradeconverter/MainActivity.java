3bed7b152f780cca2e01996eaef8ba5edbaa1888
cssmith <chssmith@roanoke.edu>
2017-02-27 02:34:47 -0500

    Simple Grade Converstion Functionality

--- app/src/main/java/edu/roanoke/cs/gradeconverter/MainActivity.java
+++ app/src/main/java/edu/roanoke/cs/gradeconverter/MainActivity.java
@@ -2,6 +2,9 @@ package edu.roanoke.cs.gradeconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        TextView userOut = (TextView)findViewById(R.id.output);
+        EditText userIn  = (EditText)findViewById(R.id.UserGrade);
+        int userGrade = Integer.parseInt(userIn.getText().toString());
+        userOut.setText(convertNumberToLetterGrade(userGrade));
+    }
+
+    /*
+     * Pre:  userGrade is a positive integer
+     * Post: Returns a string, the conversion of the usersGrade into a letter using the standard
+     *       grading scale.
+     */
+    public String convertNumberToLetterGrade(int userGrade) {
+        String letterGrade = "F";
+        if(userGrade > 90) {
+            letterGrade = "A";
+        } else if (userGrade > 80) {
+            letterGrade = "B";
+        } else if (userGrade > 70) {
+            letterGrade = "C";
+        } else {
+            letterGrade = "D";
+        }
+
+        return letterGrade;
     }
 }
---
+++ app/src/test/java/edu/roanoke/cs/gradeconverter/GradeConverstionUnitTest.java
@@ -0,0 +1,19 @@
+package edu.roanoke.cs.gradeconverter;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class GradeConverstionUnitTest {
+
+    @Test
+    public void gradeConversion_SimpleA_ReturnsA() throws Exception {
+        MainActivity testActivity = new MainActivity();
+        assertEquals("A", testActivity.convertNumberToLetterGrade(100));
+    }
+}
